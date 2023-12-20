00abbc5bcd94c498f2c257cc6dc493790d4e6f64
shaporen <shaporen@yandex.ru>
2023-12-20 22:09:38 +0300

    3. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,23 +1,49 @@
 package ru.job4j.chess;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.KingBlack;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class LogicTest {
 
     @Test
-    public void whenMoveThenFigureNotFoundException()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void whenMoveThenFigureNotFoundException() {
         Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.A2));
         FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
             logic.move(Cell.C1, Cell.H6);
         });
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    public void whenMoveThenImpossibleMoveException() {
+        Cell first = Cell.A8;
+        Cell second = Cell.G1;
+        Logic  logic = new Logic();
+        logic.add(new BishopBlack(first));
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
+            logic.move(first, second);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Could "
+                + "not way by diagonal from %s to %s", first, second);
+    }
+
+    @Test
+    public void whenMoveThenOccupiedCellException() {
+        Cell firstBB = Cell.B7;
+        Cell secondBB = Cell.G2;
+        Cell firstKB = Cell.D5;
+        Logic  logic = new Logic();
+        logic.add(new BishopBlack(firstBB));
+        logic.add(new KingBlack(firstKB));
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
+            logic.move(firstBB, secondBB);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Cell is occupied");
     }
 }
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,67 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class BishopBlackTest {
+
+    @Test
+    public void whenPositionIsCorrect() {
+        Cell first = Cell.A1;
+        BishopBlack bishopBlack = new BishopBlack(first);
+        Cell pos = bishopBlack.position();
+        assertThat(pos).isEqualTo(first);
+    }
+
+    @Test
+    public void whenCopyIsCorrect() {
+        Cell first = Cell.A1;
+        BishopBlack bishopBlack = new BishopBlack(first);
+        Figure copy = bishopBlack.copy(first);
+        assertThat(copy.position()).isEqualTo(bishopBlack.position());
+    }
+
+    @Test
+    public void whenIsDiagonal() {
+        Cell first = Cell.A7;
+        BishopBlack bishopBlack = new BishopBlack(first);
+        Cell second = Cell.E3;
+        boolean result = bishopBlack.isDiagonal(first, second);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenIsNotDiagonal() {
+        Cell first = Cell.A7;
+        BishopBlack bishopBlack = new BishopBlack(first);
+        Cell second = Cell.E5;
+        boolean result = bishopBlack.isDiagonal(first, second);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenWayIsCorrect() {
+        Cell first = Cell.A7;
+        BishopBlack bishopBlack = new BishopBlack(first);
+        Cell second = Cell.G1;
+        Cell[] result = bishopBlack.way(second);
+        assertThat(result).containsExactly(new Cell[]{Cell.B6, Cell.C5, Cell.D4, Cell.E3, Cell.F2, Cell.G1});
+    }
+
+    @Test
+    public void whenWayIsNotCorrect() {
+        Cell first = Cell.A7;
+        BishopBlack bishopBlack = new BishopBlack(first);
+        Cell second = Cell.F1;
+        ImpossibleMoveException exception = assertThrows(
+            ImpossibleMoveException.class,() -> bishopBlack.way(second)
+        );
+        assertThat(exception.getMessage()).isEqualTo(
+                "Could not way by diagonal from %s to %s", first, second);
+    }
+}
